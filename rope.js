d5e030a29cf8a777c661367c362908df85c87379
RupsaBiswas <66288687+RupsaBiswas@users.noreply.github.com>
2020-08-04 19:33:48 -0500

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,33 @@
+class Rope{
+    constructor(body1, body2,offsetX,offsetY){
+       
+       this.offsetX=offsetX;
+       this.offsetY=offsetY;
+       
+        var options = {
+            bodyA: body1,
+            bodyB: body2,
+            pointB:{x:this.offsetX,y:this.offsetY}
+
+        }
+        
+        this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+    }
+
+
+    
+
+
+    display(){
+        
+        var body1 = this.rope.body1.position;
+        var body2 = this.body2;
+        strokeWeight(4);
+        line(body1.x, body2.y, body2.x, body.y);
+    }
+}
+
+
+    
+
